72cc01a151b211722fb7264929631069007a4e64
kuipid01 <kuipid01@gmnail.com>
2023-12-19 22:53:58 +0100

    eye toggle

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,39 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { AiOutlineArrowLeft, AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
 const Register = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+  const navigate = useNavigate();
   const textAnim = {
     initial: {
-     opacity:0,
-    x:-30
+      opacity: 0,
+      x: -30,
     },
     animate: {
-       x:0,
+      x: 0,
       opacity: 1,
     },
   };
   return (
-    <div className="w-full rounded-r-3xl bg-primary-light   flex h-screen">
+    <div className="w-full relative rounded-r-3xl bg-primary-light   flex h-screen">
+           <AiOutlineArrowLeft  onClick={() => navigate(-1)} className="absolute cursor-pointer z-30 text-2xl text-gray-50 left-5 top-5" />
       <div className="hidden md:flex w-1/3 relative h-full text-white p-4 ">
-        <motion.h1  variants={textAnim}
+        <motion.h1
+          variants={textAnim}
           initial="initial"
-          animate="animate" 
+          animate="animate"
           transition={{
             duration: 1,
             type: "linear",
-         
-          }}  className=" mt-[30px] text-3xl">
-          Start your <br /> journey on <span className=" text-4xl uppercase text-purple-500 font-extrabold">kanlearn</span> kanlearn
+          }}
+          className=" mt-[50px] text-3xl"
+        >
+          Start your <br /> journey on{" "}
+          <span className=" text-4xl uppercase text-purple-500 font-extrabold">
+            kanlearn
+          </span>{" "}
+          kanlearn
         </motion.h1>
         <motion.img
-         variants={textAnim}
-         initial="initial"
-         animate="animate" 
-         transition={{
-           duration: 1,
-           type: "linear",
-        
-         }}
+          variants={textAnim}
+          initial="initial"
+          animate="animate"
+          transition={{
+            duration: 1,
+            type: "linear",
+          }}
           className=" absolute top-[20%] -right-[40%] rotate-12"
           src="/register.png"
           alt=""
@@ -45,47 +57,96 @@ const Register = () => {
           <div className=" w-full flex gap-3">
             <div className=" font-light cursor-pointer border p-1 md:p-2 flex gap-3 items-center flex-1">
               <img src="/go.png" alt="" />
-              <span className=" text-[12px] md:text-lg capitalize">Sign Up with google</span>
+              <span className=" text-[12px] md:text-lg capitalize">
+                Sign Up with google
+              </span>
             </div>
             <div className=" cursor border p-1 md:p-2 cursor-pointer font-light gap-3 flex items-center flex-1">
               <img src="/face.png" alt="" />
-              <span  className="md:text-lg capitalize text-[10px]"> Sign Up with facebook</span>
+              <span className="md:text-lg capitalize text-[10px]">
+                {" "}
+                Sign Up with facebook
+              </span>
             </div>
           </div>
-          <form className=" w-full flex flex-col gap-4" >
-            <motion.input  variants={textAnim}
-         initial="initial"
-         animate="animate" 
-         transition={{
-           duration: 1,
-           type: "linear",
-        
-         }}  type="text" className="w-full outline-none border-b border-gray-400 py-3" placeholder="Username" />
-            <motion.input  variants={textAnim}
-         initial="initial"
-         animate="animate" 
-         transition={{
-           duration: 1,
-           type: "linear",
-        
-         }}  type="email" className="w-full outline-none border-b border-gray-400 py-3" placeholder="Email" />
-            <motion.input  variants={textAnim}
-         initial="initial"
-         animate="animate" 
-         transition={{
-           duration: 1,
-           type: "linear",
-        
-         }}  type="password" className="w-full outline-none border-b border-gray-400 py-3" placeholder="Password" />
-            <button type="submit" className="bg-primary-light rounded-md text-white w-full py-3">
+          <form className=" w-full flex flex-col gap-4">
+            <motion.input
+              variants={textAnim}
+              initial="initial"
+              animate="animate"
+              transition={{
+                duration: 1,
+                type: "linear",
+              }}
+              type="text"
+              className="w-full outline-none border-b border-gray-400 py-3"
+              placeholder="Username"
+            />
+            <motion.input
+              variants={textAnim}
+              initial="initial"
+              animate="animate"
+              transition={{
+                duration: 1,
+                type: "linear",
+              }}
+              type="email"
+              className="w-full outline-none border-b border-gray-400 py-3"
+              placeholder="Email"
+            />
+            <div className="relative ">
+            <motion.input
+              variants={textAnim}
+              initial="initial"
+              animate="animate"
+              transition={{
+                duration: 1,
+                type: "linear",
+              }}
+              type={showPassword ? "text" : "password"}
+              className="w-full outline-none border-b border-gray-400 py-3"
+              placeholder="Password"
+            /> 
+              <div
+                onClick={handleTogglePasswordVisibility}
+                className="absolute  w-fit h-[20px] cursor-pointer top-1/2 -translate-y-1/2  right-2 flex items-center focus:outline-none"
+              >
+                {showPassword ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+              </div>
+            </div>
+            <div className="relative ">
+            <motion.input
+              variants={textAnim}
+              initial="initial"
+              animate="animate"
+              transition={{
+                duration: 1,
+                type: "linear",
+              }}
+              type={showPassword ? "text" : "password"}
+              className="w-full outline-none border-b border-gray-400 py-3"
+              placeholder="Confirm password"
+            /> 
+              <div
+                onClick={handleTogglePasswordVisibility}
+                className="absolute  w-fit h-[20px] cursor-pointer top-1/2 -translate-y-1/2  right-2 flex items-center focus:outline-none"
+              >
+                {showPassword ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="bg-primary-light rounded-md text-white w-full py-3"
+            >
               Create Account
             </button>
-
           </form>
           <p>
-            Already have an account? <Link className="text-primary-light" to='/login'>Log In</Link>
+            Already have an account?{" "}
+            <Link className="text-primary-light" to="/login">
+              Log In
+            </Link>
           </p>
-         
         </div>
       </div>
     </div>
